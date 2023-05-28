435d8c3eedb5ea5ac0f6406be33d4362603e05f0
ADI9325 <112470919+ADI9325@users.noreply.github.com>
2023-05-29 01:03:36 +0530

    bfs.cpp

--- bfs.cpp
+++ bfs.cpp
@@ -1,27 +1,15 @@
-// C++ code to print BFS traversal from a given
-// source vertex
+
 
 #include <bits/stdc++.h>
 using namespace std;
 
-// This class represents a directed graph using
-// adjacency list representation
 class Graph {
-
-	// No. of vertices
 	int V;
-
-	// Pointer to an array containing adjacency lists
 	vector<list<int> > adj;
 
 public:
-	// Constructor
 	Graph(int V);
-
-	// Function to add an edge to graph
 	void addEdge(int v, int w);
-
-	// Prints BFS traversal from a given source s
 	void BFS(int s);
 };
 
@@ -33,33 +21,25 @@ Graph::Graph(int V)
 
 void Graph::addEdge(int v, int w)
 {
-	// Add w to v’s list.
 	adj[v].push_back(w);
 }
 
 void Graph::BFS(int s)
 {
-	// Mark all the vertices as not visited
 	vector<bool> visited;
 	visited.resize(V, false);
 
-	// Create a queue for BFS
 	list<int> queue;
 
-	// Mark the current node as visited and enqueue it
 	visited[s] = true;
 	queue.push_back(s);
 
 	while (!queue.empty()) {
 		
-		// Dequeue a vertex from queue and print it
 		s = queue.front();
 		cout << s << " ";
 		queue.pop_front();
 
-		// Get all adjacent vertices of the dequeued
-		// vertex s. If a adjacent has not been visited,
-		// then mark it visited and enqueue it
 		for (auto adjacent : adj[s]) {
 			if (!visited[adjacent]) {
 				visited[adjacent] = true;
@@ -69,10 +49,9 @@ void Graph::BFS(int s)
 	}
 }
 
-// Driver code
 int main()
 {
-	// Create a graph given in the above diagram
+	
 	Graph g(4);
 	g.addEdge(0, 1);
 	g.addEdge(0, 2);
